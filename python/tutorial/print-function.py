0296f6634249311b88aeab2f1a2ddb4c21381202
André Pereira <andrepereira97@yahoo.com>
2022-07-31 18:04:57 -0300

    :sparkles: add python tutorial code

---
+++ python/tutorial/print-function.py
@@ -0,0 +1,8 @@
+# https://www.hackerrank.com/challenges/python-print/problem
+
+if __name__ == '__main__':
+  n = int(input())
+  output = ''
+  for i in range(1, n + 1):
+    output += str(i)
+  print(output)
