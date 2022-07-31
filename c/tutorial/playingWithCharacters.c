235ef70aa7f946daa5e2de73c83099b606582158
André Pereira <andrepereira97@yahoo.com>
2022-07-31 14:30:56 -0300

    :sparkles: add c tutorial code

---
+++ c/tutorial/playingWithCharacters.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+  char ch, s[100], sen[100];
+  
+  scanf("%c", &ch);
+  scanf("\n");
+  scanf("%[^\n]%*c", &s);
+  scanf("\n");
+  scanf("%[^\n]%*c", &sen);
+  
+  
+  printf("%c\n", ch);
+  printf("%s\n", s);
+  printf("%s\n", sen);
+  return 0;
+}
