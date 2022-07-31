c93915ce8e0032c3eca48b16c13f59de59c80658
André Pereira <andrepereira97@yahoo.com>
2022-07-31 13:31:41 -0300

    :sparkles: add ruby tutorial code

---
+++ ruby/tutorial.rb
@@ -0,0 +1,20 @@
+# https://www.hackerrank.com/challenges/ruby-hello-world/problem
+
+print "Hello HackerRank!"
+print "\n";
+
+# https://www.hackerrank.com/challenges/ruby-tutorial-everything-is-an-object/problem
+
+print self
+print "\n";
+
+# https://www.hackerrank.com/challenges/ruby-tutorial-object-methods/problem
+
+def odd_or_even(number)
+
+  number.even?
+
+end
+
+print odd_or_even 30
+print "\n";
