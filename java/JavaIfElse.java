901ef50ec2fdf83e0f5896206585fe064b1b3e9f
André Pereira <andrepereira97@yahoo.com>
2022-07-31 17:05:50 -0300

    :art: adjust identatation

--- java/JavaIfElse.java
+++ java/JavaIfElse.java
@@ -10,26 +10,24 @@ import java.util.regex.*;
 
 public class Solution {
 
+  private static final Scanner scanner = new Scanner(System.in);
 
-
-    private static final Scanner scanner = new Scanner(System.in);
-
-    public static void main(String[] args) {
-        int N = scanner.nextInt();
-        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
-        
-        if(N % 2 != 0) {
-          System.out.println("Weird");
-        } else {
-          if(N >= 2 && N <= 5) {
-            System.out.println("Not Weird");
-          } else if (N >= 6 && N <= 20) {
-            System.out.println("Weird");
-          } else if (N > 20) {
-            System.out.println("Not Weird");
-          }
-        }
-
-        scanner.close();
+  public static void main(String[] args) {
+    int N = scanner.nextInt();
+    scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+    
+    if (N % 2 != 0) {
+      System.out.println("Weird");
+    } else {
+      if (N >= 2 && N <= 5) {
+        System.out.println("Not Weird");
+      } else if (N >= 6 && N <= 20) {
+        System.out.println("Weird");
+      } else if (N > 20) {
+        System.out.println("Not Weird");
+      }
     }
+
+    scanner.close();
+  }
 }
