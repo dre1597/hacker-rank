e88dbdf146c9e02567f3c7d382534d5b5e9d1bc0
André Pereira <andrepereira97@yahoo.com>
2022-08-05 22:51:46 -0300

    :sparkles: add some java introduction challenges

---
+++ java/introduction/LoopsII.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    int q = scanner.nextInt();
+    
+    for (int i = 0; i < q; i++) {
+      int a = scanner.nextInt();
+      int b = scanner.nextInt();
+      int n = scanner.nextInt();
+      
+      int series = a;
+      
+      for(int j = 0; j < n; j++) {
+        series += Math.pow(2, j) * b;
+        System.out.printf("%s ", series);
+      }
+      
+      System.out.println();
+    }
+    
+    scanner.close(); 
+  }
+}
